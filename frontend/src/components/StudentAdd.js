830a64d4fe4d099ee7737dc5ab0ca80f7d8ba7bc
unknown <nik22517@gmail.com>
2022-12-25 21:10:31 +0530

    commit

--- frontend/src/components/StudentAdd.js
+++ frontend/src/components/StudentAdd.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
+import Loader from "./Loader";
 
 const StudentAdd = ({ setLoading, loading }) => {
   const [studentDetail, setStudentDetail] = useState({
@@ -107,6 +108,7 @@ const StudentAdd = ({ setLoading, loading }) => {
           onChange={handleChange}
         />
         <input type={"submit"} value='Add' onClick={clickHandle} />
+        {loading ? <Loader /> : ""}
       </AddForm>
     </>
   );
